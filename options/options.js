0270e1f4827b44017b0cab142f25901371762fe5
Crowdbotics <engineering@crowdbotics.com>
2023-08-13 23:11:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "crimson-union-dev-108926",
+  url: "https://crimson-union-dev-108926.botics.co",
+  api: "https://crimson-union-dev-108926.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
